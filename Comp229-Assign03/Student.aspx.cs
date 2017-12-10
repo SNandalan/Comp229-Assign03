f57f75fee363d9416adcad6d745981ae16c3bc00
SNandalan <snandal2@my.centennialcollege.ca>
2017-12-09 23:32:36 -0500

    Added values to table in Student Page

--- Comp229-Assign03/Student.aspx.cs
+++ Comp229-Assign03/Student.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +13,46 @@ namespace Comp229_Assign03
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ConnectData();
+            ConnectCourseData();
+        }
+        private void ConnectData()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Comp229-Assign03"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand comm = new SqlCommand("select * from Students", conn);
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                studentGridView.DataSource = reader;
+                studentGridView.DataBind();
+                reader.Close();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        private void ConnectCourseData()
+        {
+            int StudentID = Convert.ToInt32(Request.QueryString["StudentID"]);
+            string connectionString = ConfigurationManager.ConnectionStrings["Comp229-Assign03"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connectionString);
+            SqlCommand comm = new SqlCommand("SELECT * FROM Courses WHERE CourseID IN (SELECT CourseID FROM Enrollments WHERE StudentID=" + StudentID + ")", conn);
+            try
+            {
+                // Open the connection
+                conn.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                coursesGridView.DataSource = reader;
+                coursesGridView.DataBind();
+                reader.Close();
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
